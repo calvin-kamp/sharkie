97f416262930dade16899edffdc2b3b1094ede12
Calvin Kamp <calvin.kamp6696@gmail.com>
2025-12-31 17:42:41 +0100

    feat(assets): add and implement luckiest guy font

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,11 @@ export default defineConfig({
         alias: {
             '@root': fileURLToPath(new URL('./src', import.meta.url)),
             '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
-            '@scripts': fileURLToPath(new URL('./src/scripts', import.meta.url)),
+            '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+            '@models': fileURLToPath(new URL('./src/models', import.meta.url)),
+            '@scripts': fileURLToPath(
+                new URL('./src/scripts', import.meta.url)
+            ),
         },
     },
 })
