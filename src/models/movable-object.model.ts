5f813d1bb8724e78a36d99bf3fc14416055e2e89
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-03 12:09:14 +0100

    feat: updated movable-object model

--- src/models/movable-object.model.ts
+++ src/models/movable-object.model.ts
@@ -4,31 +4,65 @@ export interface MovableObjectConfig {
     y?: number
     width?: number
     height?: number
+    aspectRatio?: number
 }
 
 export class MovableObject {
     x: number
     y: number
     width: number
-    height: number
+    height?: number
+    private aspectRatio: number
     img: HTMLImageElement
+    directionLeft: boolean = false
+    cachedImages: HTMLImageElement[] = []
 
     constructor(config: MovableObjectConfig) {
         this.img = new Image()
 
         this.x = config.x ?? 0
         this.y = config.y ?? 0
+
+        this.aspectRatio = config.aspectRatio ?? 2 / 3
+
         this.width = config.width ?? 100
-        this.height = config.height ?? 150
+        this.height = config.height ?? undefined
 
         this.loadImage(config.imageSrc)
     }
 
-    loadImage(imagePath: string) {
+    private loadImage(imagePath: string) {
         this.img.src = imagePath
     }
 
-    moveLeft() {}
+    cacheImages(images: string[]) {
+        for(const imageSrc of images) {
+            const img = new Image()
+            img.src = imageSrc
 
-    moveRight() {}
+            this.cachedImages.push(img)
+        }
+    }
+
+    moveUp() {
+        this.y -= 2.5
+    }
+
+    moveDown() {
+        this.y += 2.5
+    }
+
+    moveLeft() {
+        this.x -= 2.5
+    }
+
+    moveRight() {
+        this.x += 2.5
+    }
+
+    get calculatedHeight() {
+
+        return this.height || Math.floor(this.width * this.aspectRatio)
+
+    }
 }
